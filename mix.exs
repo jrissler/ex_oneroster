b29ad6b64ee48952883aa13ce5f72ced3176a57a
James Rissler <risslerjames@gmail.com>
2018-11-30 15:54:48 -0600

    Start bringing back to life.

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule ExOneroster.Mixfile do
   def project do
     [app: :ex_oneroster,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.7.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      start_permanent: Mix.env == :prod,
      aliases: aliases(),
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+     preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      preferred_cli_env: [vcr: :test, "vcr.delete": :test, "vcr.check": :test, "vcr.show": :test],
      deps: deps()]
   end
@@ -32,22 +32,22 @@ defmodule ExOneroster.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_ecto, "~> 4.0.0"},
+      {:postgrex, ">= 0.14.1"},
+      {:phoenix_html, "~> 2.12"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 0.11.2"},
-      {:poison, "~> 3.0"},
+      {:poison, "~> 3.1"},
       {:uuid, "~> 1.1"},
-      {:json_web_token, "~> 0.2"},
-      {:ex_machina, "~> 2.0", only: :test},
-      {:excoveralls, "~> 0.6", only: :test},
-      {:exvcr, "~> 0.8", only: :test},
-      {:credo, "~> 0.7.4", only: [:dev, :test], runtime: false}
+      {:json_web_token, "~> 0.2.10"},
+      {:ex_machina, "~> 2.1", only: :test},
+      {:excoveralls, "~> 0.6.3", only: :test},
+      {:exvcr, "~> 0.8.8", only: :test},
+      {:credo, "~> 0.9.2", only: [:dev, :test], runtime: false}
     ]
   end
 
